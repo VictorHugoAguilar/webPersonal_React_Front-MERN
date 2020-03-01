3fdd11c9f7c9fd6cf53a6c33a4fc5b1f7eb55f27
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-03-01 19:39:11 +0100

    Añadir servicio para traer users

--- src/pages/Admin/Users/Users.js
+++ src/pages/Admin/Users/Users.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { getAccessTokenApi } from '../../../api/auth';
+import { getUsersActiveApi } from '../../../api/user'
 
 import './Users.scss';
 
 export default function Users() {
+    const [usersActive, setUsersActive] = useState([]);
+    const [usersInactive, setUsersInactive] = useState([]);
+    const token = getAccessTokenApi();
+
+    useEffect(() => {
+        getUsersActiveApi(token, true)
+            .then(response => {
+                setUsersActive(response);
+            });
+        getUsersActiveApi(token, false)
+            .then(response => {
+                setUsersInactive(response);
+            });
+    }, [token]);
+
     return (<div>Lista de usuarios</div>)
 }
