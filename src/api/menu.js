5dbf9a232430868047ac50456a063dcc9249f4b0
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-03-08 21:51:15 +0100

    Añadir funcion de añadir menu, componente, y api

--- src/api/menu.js
+++ src/api/menu.js
@@ -80,3 +80,62 @@ export function updateMenuApi(token, menuId, data) {
             return error;
         })
 }
+
+export function activateMenuApi(token, menuId, status) {
+    const url = `${basePath}${apiVersion}/activate-menu/${menuId}`;
+
+    const params = {
+        method: 'PUT',
+        body: JSON.stringify({ active: status }),
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: token
+        },
+    };
+
+    return fetch(url, params)
+        .then(response => {
+            return response.json();
+        })
+        .then(result => {
+            if (result.ok) {
+                return result;
+            }
+            console.error(result);
+            return result;
+        })
+        .catch(error => {
+            console.error('error', error)
+            return error;
+        })
+}
+
+export function addMenuApi(token, menu) {
+    const url = `${basePath}${apiVersion}/add-menu`;
+
+    const params = {
+        method: 'POST',
+        body: JSON.stringify(menu),
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: token
+        },
+    };
+
+    return fetch(url, params)
+        .then(response => {
+            return response.json();
+        })
+        .then(result => {
+            if (result.ok) {
+                return result;
+            }
+            console.error(result);
+            return result;
+        })
+        .catch(error => {
+            console.error('error', error)
+            return error;
+        })
+
+}
--- src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
+++ src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Switch, List, Button, Icon, Modal as ModalAntd } from 'antd';
-import Modal from '../../../Modal';
+import { Switch, List, Button, Icon, Modal as ModalAntd, notification } from 'antd';
 import DragSortableList from 'react-drag-sortable'
 
+import Modal from '../../../Modal';
+import AddMenuWebForm from '../../../../components/Admin/MenuWeb/AddMenuWebForm';
+
 // Importar api para conectar con el back
-import { updateMenuApi } from '../../../../api/menu';
+import { updateMenuApi, activateMenuApi } from '../../../../api/menu';
 import { getAccessTokenApi } from '../../../../api/auth';
 
 
@@ -22,49 +24,92 @@ export default function MenuWebList(props) {
         const listItemArray = [];
 
         Array.prototype.forEach.call(menu, item => {
-            console.log(item)
             listItemArray.push({
-                content: <MenuItem item={item} />
+                content: <MenuItem item={item} activateMenu={activateMenu} />
             })
         })
         setListItems(listItemArray);
     }, [menu]);
 
+
+    const activateMenu = (menu, status) => {
+        const accessToken = getAccessTokenApi();
+
+        activateMenuApi(accessToken, menu._id, status)
+            .then(response => {
+                notification['success']({
+                    message: response.message
+                });
+            })
+            .catch(err => {
+                notification['error']({
+                    message: err.message
+                });
+            });
+    };
+
+
     const onSort = (sortedList, dropEvent) => {
-        console.log(sortedList);
         const accessToken = getAccessTokenApi();
 
         sortedList.forEach(item => {
             const { _id } = item.content.props.item;
             const { order } = item.rank;
             updateMenuApi(accessToken, _id, { order })
-                .then(result => { console.log(result) })
-                .catch(err => { console.error(err) });
-        })
-    }
+                .then(result => console.log(result.message))
+                .catch(err => console.error(err.message))
+        });
+    };
+
+    const addMenuWebModal = () => {
+        setIsVisibleModal(true);
+        setModalTitle('Creando nuevo menú');
+        setModalContent(
+            <div>
+                <AddMenuWebForm 
+                    setIsVisibleModal={setIsVisibleModal}
+                    setReloadMenuWeb={setReloadMenuWeb}
+                />
+            </div>
+        );
+    };
 
     return (
         <div className="menu-web-list">
             <div className="menu-web-list__header">
-                <Button type="primary" className="menu-web-list__header__button">Menu menú</Button>
+                <Button
+                    type="primary"
+                    className="menu-web-list__header__button"
+                    onClick={addMenuWebModal}
+                >
+                    <Icon type="plus" />
+                    Añadir menú
+                </Button>
             </div>
 
             <div className="menu-web-list__items">
                 <DragSortableList items={listItems} onSort={onSort} type="vertical" />
             </div>
+
+            <Modal
+                title={modalTitle}
+                isVisible={isVisibleModal}
+                setIsVisible={setIsVisibleModal}
+            >
+                {modalContent}
+            </Modal>
         </div>
     );
-
 }
 
 function MenuItem(props) {
-    const { item } = props;
+    const { item, activateMenu } = props;
 
     return (
         <List.Item
             actions={
                 [
-                    <Switch defaultChecked={item.active} />,
+                    <Switch defaultChecked={item.active} onChange={e => activateMenu(item, e)} />,
                     <Button type="primary">
                         <Icon type="edit"></Icon>
                     </Button>,
