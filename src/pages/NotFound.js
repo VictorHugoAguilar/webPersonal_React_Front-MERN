396bcdefda415a9562d29db099e83086d649cd5f
victorHugoAguilar <victorhugoaguilar@aol.com>
2020-03-14 13:31:23 +0100

    Arreglo de not-found

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Result, Button } from 'antd';
 
+
 import '../scss/index.scss'
 
 export default function NotFound() {
+
+  const redirect = () => {
+    window.location.href = "/";
+  }
+
     return (
         <Result
         className="text-white"
         status="404"
         title="404"
         subTitle="Sorry, the page you visited does not exist."
-        extra={<Button type="primary">Back Home</Button>}
+        extra={<Button onClick={ () => redirect()  } type="primary">Volver al inicio</Button>}
       />
     );
 }
